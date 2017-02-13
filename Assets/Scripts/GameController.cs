a6fdb23823c7179ce94cd9717b61b93b57b601c6
doubi loli <503871002@qq.com>
2017-02-13 15:58:51 +0800

    Space Shooter
    Unity3D官方教程练习

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public GameObject hazard;
+    public Vector3 spawnValues;
+
+    private void Start()
+    {
+        SpawnWaves();
+    }
+
+    void SpawnWaves()
+    {
+        Vector3 spawnPosision = new Vector3(Random.Range(-spawnValues.x,spawnValues.x),spawnValues.y,spawnValues.z);
+        Quaternion spawnRotation = Quaternion.identity;
+        Instantiate(hazard, spawnPosision, spawnRotation);
+    }
+}
